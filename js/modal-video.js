752e90a24c802fe04be2907c5c6f02c0783fd3c1
unknown <milos.milijasevic00@gmail.com>
2023-05-12 19:02:06 +0200

    modal-video-js updated

---
+++ js/modal-video.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector(".modal-video");
+const openButton = document.querySelector("#open-button");
+const closeButton = document.querySelector("#close-button");
+
+
+const openModal = () =>{
+    modal.style.visibility = "visible";
+    modal.style.opacity = "1";
+};
+
+const closeModal = () =>{
+    modal.style.visibility = "hidden";
+    modal.style.opacity = "0";
+}
+
+openButton.addEventListener("click", () =>{ openModal(); });
+
+closeButton.addEventListener("click", () =>{ closeModal(); });
+
+modal.addEventListener("click", () =>{ closeModal(); });
