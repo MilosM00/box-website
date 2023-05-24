ded4b2e678c8591a351fe2cc2eaff3aeb6b4be52
unknown <milos.milijasevic00@gmail.com>
2023-05-24 16:47:45 +0200

    thank-you-php updated

---
+++ thank-you.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bitter&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/2c28c53cee.js" crossorigin="anonymous" defer></script>
+    <link rel="stylesheet" href="style/thank-you.css">
+    <script src="js/hamburger.js" defer></script>
+    <title>Box Website - Thank You</title>
+</head>
+<body>
+    
+    <?php include("navbar.php"); ?>
+
+    
+    <div class="container-thank-you">
+        <i class="fa fa-check"></i>
+        <p>Thank you for your time!</p>
+        <p>You bought the boxing package successfuly!</p>
+    </div>
+
+    
+    <?php include("footer.php"); ?>
+
+</body>
+</html>
