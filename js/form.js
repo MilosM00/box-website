337f8d6d75959fd273966932b74f7db06638b9a9
unknown <milos.milijasevic00@gmail.com>
2023-05-28 16:41:17 +0200

    form-js updated

---
+++ js/form.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("#form");
+
+const labelFirstName = document.querySelector("#label-first-name");
+const labelLastName = document.querySelector("#label-last-name");
+const labelPhoneNumber = document.querySelector("#label-phone-number");
+const labelEmailAddress = document.querySelector("#label-email-address");
+
+const inputFirstName = document.querySelector("#input-first-name");
+const inputLastName = document.querySelector("#input-last-name");
+const inputPhoneNumber = document.querySelector("#input-phone-number");
+const inputEmailAddress = document.querySelector("#input-email-address");
