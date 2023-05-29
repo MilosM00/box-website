57f53dc3bcaf0ee6114d0055b84c6cea43775b83
unknown <milos.milijasevic00@gmail.com>
2023-05-29 19:18:43 +0200

    form-js updated

--- js/form.js
+++ js/form.js
@@ -9,3 +9,66 @@ const inputFirstName = document.querySelector("#input-first-name");
 const inputLastName = document.querySelector("#input-last-name");
 const inputPhoneNumber = document.querySelector("#input-phone-number");
 const inputEmailAddress = document.querySelector("#input-email-address");
+
+
+form.addEventListener("submit", (e) =>{
+    if(inputFirstName.value.length < 2 || !inputFirstName.value.match(/^[A-Za-z]+$/))
+    {
+        e.preventDefault();
+        labelFirstName.style.color = "red";
+        labelFirstName.textContent = "Please, enter a first name!";
+        inputFirstName.value = "";
+    }
+
+    else
+    {
+        labelFirstName.style.color = "#fff";
+        labelFirstName.textContent = "First Name ✔";
+    }
+
+    
+    if(inputLastName.value.length < 2 || !inputLastName.value.match(/^[A-Za-z]+$/))
+    {
+        e.preventDefault();
+        labelLastName.style.color = "red";
+        labelLastName.textContent = "Please, enter a last name!";
+        inputLastName.value = "";
+    }
+
+    else
+    {
+        labelLastName.style.color = "#fff";
+        labelLastName.textContent = "Last Name ✔";
+    }
+
+
+    if(inputEmailAddress.value.length < 5 || !String(inputEmailAddress.value).includes("@"))
+    {
+        e.preventDefault();
+        labelEmailAddress.style.color = "red";
+        labelEmailAddress.textContent = "Please, enter a e-mail address!";
+        inputEmailAddress.value = "";
+    }
+
+    else
+    {
+        labelEmailAddress.style.color = "#fff";
+        labelEmailAddress.textContent = "E-mail Address ✔";   
+    }
+
+    
+    if(inputPhoneNumber.value.length < 5 || inputPhoneNumber.value.match(/^[A-Za-z]+$/))
+    {
+        e.preventDefault();
+        labelPhoneNumber.style.color = "red";
+        labelPhoneNumber.textContent = "Please, enter a phone number!";
+        inputPhoneNumber.value = "";
+    }
+    
+    else
+    {
+        labelPhoneNumber.style.color = "#fff";
+        labelPhoneNumber.textContent = "Phone Number ✔";
+    }
+
+});
